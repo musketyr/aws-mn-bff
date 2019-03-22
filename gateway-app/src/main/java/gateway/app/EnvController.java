9034a878c349d1b518c99f34a3dada76910d1a72
Vladimir Orany <vladimir@orany.cz>
2019-03-22 09:56:18 +0100

    API Gateway + SAM

---
+++ gateway-app/src/main/java/gateway/app/EnvController.java
@@ -0,0 +1,22 @@
+package gateway.app;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import java.util.Set;
+
+@Controller("/env")
+public class EnvController {
+
+    private final ApplicationContext applicationContext;
+
+    public EnvController(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Get("/")
+    public Set<String> index() {
+        return applicationContext.getEnvironment().getActiveNames();
+    }
+}
